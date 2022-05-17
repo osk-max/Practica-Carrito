7fed159fecd7dda2e7940643799302b5ebd95d01
Oscar Cantor <ocantorb1889@gmail.com>
2022-05-17 04:38:33 +0200

    Se inicia de nuevo

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// Variables inicio 04 mayo 2022
-const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
-const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const listaCursos = document.querySelector("#lista-cursos");
-let articulosCarrito = [];
-
-
-
