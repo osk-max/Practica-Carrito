73f828fac31bf810be7570c6e3d3baf15a915139
Oscar Cantor <ocantorb1889@gmail.com>
2022-06-07 01:37:45 +0200

    Practicando

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// VARIABLES
-const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
-const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const listaCursos = document.querySelector('#lista-cursos');
-
-// EVENTOS
